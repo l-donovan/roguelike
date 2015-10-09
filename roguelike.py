a219716347fead2866a0fe2593850d10a0252a17
l-donovan <ladbuilder1@gmail.com>
2015-10-08 22:21:09 -0400

    entirely refactored - old support will be removed next commit

--- roguelike.py
+++ roguelike.py
@@ -1,24 +1,19 @@
 #/usr/bin/env python
+
+# TODO: Horizontal collision detection, everything else
+
 import os, pygame
 from pygame.locals import *
-from random import randint
-import utils
+from random import randint, choice
+from utils import loadSpritesheet, loadCharacterSpritesheet
+from models import Level, Tile, Entity
 
-playerType = 'rogue'
-
-tRes = 16 # Tile resolution, single height tiles are 16x16 px, double height tiles are 16x32 px, double width and height tiles are 32x32
-tW, tH = 16, 16 # Tiles per screen in width and height
-gameScale = 2 # Upscaling multiplier, also affects movement speed
-characterScale = 2 # Character upscaling multiplier, i.e. characters are 32x32 when tiles are 16x16
-drawLevels = (0, 100) # Range of different rendering levels allowed
-
-xRes, yRes = gameScale * tRes * tW, gameScale * tRes * tH
-drawOrder = [[]] * (drawLevels[1] - drawLevels[0])
-
-level = [
+playerType = 'goblin'
+gameScale = 2
+tileData = [
             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-            ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'],
-            ['w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
             ['W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W'],
             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
@@ -26,125 +21,82 @@ level = [
             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-            ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'],
-            ['w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'],
+            [' ',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
             ['W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W'],
         ]
+decorationData = [
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','c',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','c',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','c',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','c',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','c',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'],
+        ]
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        self.x, self.y, self.dx, self.dy = x, y, 0, 0
-        self.isMoving = False
-        self.isCasting = False
-        self.isFacingRight = True
-        pygame.sprite.Sprite.__init__(self)
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
-
-    def update(self):
-        xTile = (int)((self.x + self.dx) / (gameScale * tRes) + characterScale)
-        yTile = (int)((self.y + self.dy) / (gameScale * tRes) + characterScale)
-
-        if (xTile > tW - 1 or yTile > tH - 1 or xTile < 0 or yTile < 0): return
-        if (level[yTile][xTile].isMovable):
-            self.x += self.dx
-            self.y += self.dy
-
-class SingleHeight(pygame.sprite.Sprite):
-    def __init__(self, unitType, drawLevel, isMovable):
-        self.unitType = unitType
-        self.drawLevel = drawLevel
-        self.isMovable = isMovable
-        self.x, self.y = 0, 0
-        pygame.sprite.Sprite.__init__(self)
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
-
-class DoubleHeight(pygame.sprite.Sprite):
-    def __init__(self, unitType, drawLevel, isMovable):
-        self.unitType = unitType
-        self.drawLevel = drawLevel
-        self.isMovable = isMovable
-        self.x, self.y = 0, 0
-        pygame.sprite.Sprite.__init__(self)
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
+xRes, yRes = (int)(gameScale * 256), (int)(gameScale * 256)
+level = Level(16, 16, 2)
+level.setAllTiles(tileData)
+level.setAllDecorations(decorationData)
 
 def loadTextures():
-    characterSpritesheet = utils.spritesheet('assets/' + playerType + '.png')
-    tileSpritesheet = utils.spritesheet('assets/dungeon.png')
-
-    wallX, wallY = 0, 4
-    ceilingX, ceilingY = 0, 1
-    floorX, floorY = 0, 7
-
     global playerTextures, wallTextures, ceilingTextures, floorTextures
 
-    playerTextures = characterSpritesheet.loadSprites(tRes * characterScale, tRes * characterScale, 10, 10, xScl=gameScale, yScl=gameScale, colorkey=-1)
-    wallTextures = tileSpritesheet.imagesAt([(wallX * tRes + i * tRes, wallY * tRes, tRes, tRes * 2) for i in range(0, 8)], xScl=gameScale, yScl=gameScale)
-    ceilingTextures = tileSpritesheet.imagesAt([(ceilingX * tRes + i * tRes, ceilingY * tRes, tRes, tRes * 2) for i in range(0, 29)], xScl=gameScale, yScl=gameScale)
-    floorTextures = tileSpritesheet.imagesAt([(floorX * tRes + i * tRes, floorY * tRes, tRes, tRes * 2) for i in range(0, 29)], xScl=gameScale, yScl=gameScale)
+    playerTextures = loadCharacterSpritesheet('assets/' + playerType + '.png', 32, 32, 10, 10, xScale=gameScale, yScale=gameScale, colorkey=-1)
+    wallTextures = loadSpritesheet('assets/wall.png', 16, 32, xScale=gameScale, yScale=gameScale)
+    ceilingTextures = loadSpritesheet('assets/ceiling.png', 16, 16, xScale=gameScale, yScale=gameScale)
+    floorTextures = loadSpritesheet('assets/floor.png', 16, 16, xScale=gameScale, yScale=gameScale)
 
-def loadLevel():
-    for i in range(0, tH):
-        for j in range(0, tW):
-            if level[i][j] == 'W':
-                w = DoubleHeight('wall', 0, False)
-                w.image = wallTextures[randint(0, 6)]
-                w.x = j * tRes * gameScale
-                w.y = (i - 1) * tRes * gameScale
-                level[i][j] = w
-            elif level[i][j] == 'c':
-                c = SingleHeight('ceiling', 0, True)
-                c.image = ceilingTextures[11] # randint(0, 28)
-                c.x = j * tRes * gameScale
-                c.y = i * tRes * gameScale
-                level[i][j] = c
-            elif level[i][j] == 'w':
-                w = SingleHeight('wallTop', 99, False)
-                w.image = wallTextures[randint(0, 6)]
-                w.x = j * tRes * gameScale
-                w.y = i * tRes * gameScale
-                level[i][j] = w
+def loadLevel(tileData, decorationData, outputLevel):
+    for y in range(0, 16):
+        for x in range(0, 16):
+            if tileData[y][x] == 'W':
+                tile = Tile('wall', False)
+                tile.image = choice(wallTextures)
+                tile.offsetY = -16 * gameScale
+            elif tileData[y][x] == 'c':
+                tile = Tile('ceiling', True)
+                tile.image = choice(ceilingTextures)
+                tile.offsetY = 0
             else:
-                f = SingleHeight('floor', 0, True)
-                f.image = floorTextures[randint(0, 28)]
-                f.x = j * tRes * gameScale
-                f.y = i * tRes * gameScale
-                level[i][j] = f
-
-def calcDrawOrder():
-    for i in range(0, tH):
-        for j in range(0, tW):
-            drawOrder[level[j][i].drawLevel].append(level[j][i])
+                tile = Tile('floor', True)
+                tile.image = choice(floorTextures)
+                tile.offsetY = 0
+            if decorationData[y][x] == 'c':
+                dec = Tile('ceiling', True)
+                dec.image = choice(ceilingTextures)
+                dec.offsetY = -16 * 2 * gameScale
+            else:
+                dec = None
+            outputLevel.setTileAt(x, y, tile)
+            outputLevel.setDecorationAt(x, y, dec)
 
 def main():
-    # Setup
     pygame.init()
     screen = pygame.display.set_mode((xRes, yRes))
     pygame.display.set_caption('Roguelike')
 
-    background = pygame.Surface(screen.get_size()).convert()
-    background.fill((80, 80, 80))
-    pygame.display.flip()
-
     animationIndex, spriteIndex = 0, 0
     clock = pygame.time.Clock()
-    player = Player(256, 256)
+    player = Entity(level)
 
     loadTextures()
-    loadLevel()
-    calcDrawOrder()
+    loadLevel(tileData, decorationData, level)
 
-    # Main Loop
     while True:
-        screen.blit(background, (0, 0))
         clock.tick(24)
 
         player.isMoving = False
@@ -154,10 +106,10 @@ def main():
         if keys[K_ESCAPE]: return
         if keys[K_UP]:
             player.isMoving = True
-            player.dy = -1 * gameScale
+            player.dy = -2 * gameScale
         if keys[K_DOWN]:
             player.isMoving = True
-            player.dy = 1 * gameScale
+            player.dy = 2 * gameScale
         if keys[K_LEFT]:
             player.isFacingRight = False
             player.isMoving = True
@@ -169,9 +121,6 @@ def main():
         if keys[K_z]:
             player.isCasting = True
             animationIndex = 0
-        if keys[K_r]:
-            shouldReload = True;
-            break
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -195,16 +144,15 @@ def main():
         if not player.isFacingRight:
             player.image = pygame.transform.flip(player.image, True, False)
 
-        for i in range(0, len(drawOrder)):
-            for j in range(0, len(drawOrder[i])):
-                drawOrder[i][j].draw(screen)
+        pX, pY = player.update()
+        for y in range(0, 16):
+            for x in range(0, 16):
+                screen.blit(level.getTileAt(x, y).image, (x * 16 * gameScale, y * 16 * gameScale + level.getTileAt(x, y).offsetY))
+                if (level.getDecorationAt(x, y) is not None):
+                    screen.blit(level.getDecorationAt(x, y).image, (x * 16 * gameScale, y * 16 * gameScale + level.getDecorationAt(x, y).offsetY))
+                if ((x, y - 1) == (pX, pY)):
+                    screen.blit(player.image, (player.x + player.offsetX, player.y + player.offsetY))
 
-        player.update()
-        player.draw(screen)
         pygame.display.flip()
-
-    if shouldReload:
-        shouldReload = False
-        main()
 
 if __name__ == '__main__': main()
--- utils.py
+++ utils.py
@@ -31,7 +31,7 @@ class spritesheet(object):
         tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
                 for x in range(image_count)]
         return self.imagesAt(tups, xScl, yScl, colorkey)
-        
+
     # Loads all the sprites in a spritesheet into a two-dimensional array
     def loadSprites(self, xDim, yDim, xNum, yNum, xScl=1, yScl=1, colorkey=None):
         output = []
@@ -48,3 +48,44 @@ class spritesheet(object):
                 images.append((image, image.get_rect()))
             output.append(images)
         return output
+
+def loadSpritesheet(filename, xRes, yRes, xScale=1, yScale=1, colorkey=None):
+    try:
+        file = pygame.image.load(filename).convert()
+    except pygame.error, message:
+        print('[FATAL] Failed to load spritesheet (' + filename + ')')
+        raise(SystemExit, message)
+
+    images = [None] * ((int)(file.get_width() / xRes))
+    for i in range(0, len(images)):
+        rect = pygame.Rect((i * xRes, 0, xRes, yRes))
+        image = pygame.Surface(rect.size).convert()
+        image.blit(file, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        images[i] = pygame.transform.scale(image, (xRes * xScale, yRes * yScale))
+    return images
+
+def loadCharacterSpritesheet(filename, xRes, yRes, xNum, yNum, xScale=1, yScale=1, colorkey=None):
+    try:
+        file = pygame.image.load(filename).convert()
+    except pygame.error, message:
+        print('[FATAL] Failed to load spritesheet (' + filename + ')')
+        raise(SystemExit, message)
+
+    output = []
+    for i in range(0, yNum):
+        images = []
+        for x in range(xNum):
+            rect = pygame.Rect((xRes * x, yRes * i, xRes, yRes))
+            image = pygame.Surface(rect.size).convert()
+            image.blit(file, (0, 0), rect)
+            if colorkey is not None:
+                if colorkey is -1: colorkey = image.get_at((0, 0))
+                image.set_colorkey(colorkey, pygame.RLEACCEL)
+            image = pygame.transform.scale(image, (xRes * xScale, yRes * yScale))
+            images.append((image, image.get_rect()))
+        output.append(images)
+    return output
